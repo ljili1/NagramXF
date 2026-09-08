3262d8ba395705df4318ed8917bbbaf5f40baf85
WorkBuddy <workbuddy@local>
2026-09-08 18:56:27 +0800

    fix(vless-settings): add missing android.view.ViewGroup import
    ListAdapter.onCreateViewHolder took ViewGroup without the import, breaking
    compileNormalReleaseJavaWithJavac.

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/settings/VlessSettingsActivity.java
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/settings/VlessSettingsActivity.java
@@ -9,6 +9,7 @@ import android.text.InputType;
 import android.text.TextUtils;
 import android.util.TypedValue;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
